6f29f9dccafca840632e85641a3ef7e234d78caf
Tornike512 <Dht5152@gmail.com>
2023-11-19 22:12:16 +0400

    created new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
+import { Symbols } from "./Symbols";
 import "./App.scss";
 
 function App() {
   return (
     <>
       <div className="background">
-        <div className="background__grid">1</div>
-        <div className="background__grid">2</div>
-        <div className="background__grid">3</div>
-        <div className="background__grid">4</div>
-        <div className="background__grid">5</div>
-        <div className="background__grid">6</div>
-        <div className="background__grid">7</div>
-        <div className="background__grid">8</div>
-        <div className="background__grid">9</div>
+        <Symbols></Symbols>
       </div>
     </>
   );
---
+++ src/Symbols/Symbols.jsx
@@ -0,0 +1,17 @@
+import "./Symbols.scss";
+
+export function Symbols() {
+  return (
+    <>
+      <div className="background__grid">1</div>
+      <div className="background__grid">2</div>
+      <div className="background__grid">3</div>
+      <div className="background__grid">4</div>
+      <div className="background__grid">5</div>
+      <div className="background__grid">6</div>
+      <div className="background__grid">7</div>
+      <div className="background__grid">8</div>
+      <div className="background__grid">9</div>
+    </>
+  );
+}
