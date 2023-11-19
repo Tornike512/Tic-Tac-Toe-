ceaa8c6b88eee55beec9fc8f9eb8e95f1231fe56
Tornike512 <Dht5152@gmail.com>
2023-11-19 22:41:44 +0400

    removed divs and added buttons instead

--- src/Symbols/Symbols.jsx
+++ src/Symbols/Symbols.jsx
@@ -3,33 +3,15 @@ import "./Symbols.scss";
 export function Symbols() {
   return (
     <>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
-      <div className="background__grid">
-        <h1 className="background__grid__x">X</h1>
-      </div>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
+      <button className="background__grid">X</button>
     </>
   );
 }
