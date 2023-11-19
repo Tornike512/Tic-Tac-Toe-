725aafdd32582fd6ed412358107660dd0273ea38
Tornike512 <Dht5152@gmail.com>
2023-11-19 22:37:34 +0400

    added x and o font sized in grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="background">
-        <Symbols></Symbols>
+        <Symbols />
       </div>
     </>
   );
--- src/Symbols/Symbols.jsx
+++ src/Symbols/Symbols.jsx
@@ -3,15 +3,33 @@ import "./Symbols.scss";
 export function Symbols() {
   return (
     <>
-      <div className="background__grid">1</div>
-      <div className="background__grid">2</div>
-      <div className="background__grid">3</div>
-      <div className="background__grid">4</div>
-      <div className="background__grid">5</div>
-      <div className="background__grid">6</div>
-      <div className="background__grid">7</div>
-      <div className="background__grid">8</div>
-      <div className="background__grid">9</div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
+      <div className="background__grid">
+        <h1 className="background__grid__x">X</h1>
+      </div>
     </>
   );
 }
