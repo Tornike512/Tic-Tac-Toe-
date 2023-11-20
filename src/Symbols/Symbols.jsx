7e1edada298af273d239e82c23f9e23a40aa6dd6
Tornike512 <Dht5152@gmail.com>
2023-11-20 21:52:19 +0400

    Added a simple logic to the project

--- src/Symbols/Symbols.jsx
+++ src/Symbols/Symbols.jsx
@@ -4,11 +4,34 @@ import "./Symbols.scss";
 export function Symbols() {
   const [values, setValues] = useState(Array(9).fill(""));
 
-  const handleButtonClick = (i) => {
+  function handleButtonClick(i) {
     const newValues = [...values];
     newValues[i] = "X";
     setValues(newValues);
-  };
+    if (
+      newValues[0] === "X" ||
+      newValues[1] === "X" ||
+      newValues[2] === "X" ||
+      newValues[3] === "X" ||
+      newValues[4] === "X" ||
+      newValues[5] === "X" ||
+      newValues[6] === "X" ||
+      newValues[7] === "X" ||
+      newValues[8] === "X"
+    ) {
+      newValues[4] = "O";
+      const newValues1 = [...newValues];
+      newValues1[i] = "X";
+      setValues(newValues1);
+      if (newValues1[2] === "X") {
+        newValues1[1] = "O";
+      } else if (newValues1[6] === "X") {
+        newValues1[3] = "O";
+      } else if (newValues1[8] === "X") {
+        newValues1[7] = "O";
+      }
+    }
+  }
 
   return (
     <>
@@ -16,7 +39,9 @@ export function Symbols() {
         <button
           key={i}
           className="background__grid"
-          onClick={() => handleButtonClick(i)}
+          onClick={() => {
+            handleButtonClick(i);
+          }}
         >
           {value}
         </button>
